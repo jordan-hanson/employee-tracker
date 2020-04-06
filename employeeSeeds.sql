aa1b11ee2ac54379f7408934b945fc7725f3350c
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-04-06 06:27:38 -0600

    added seeds table

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -3,3 +3,14 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE employee_tracker (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    salary INT default 0,
+    manager VARCHAR(50) NULL,
+    PRIMARY KEY(id)
+);
